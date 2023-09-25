c7e9c4215199a7f27a76379bdc8c34746bb90b37
jakewilliams33 <jakewilliams33@hotmail.com>
2023-09-26 00:06:41 +0100

    configure netlify identity

--- app/page.js
+++ app/page.js
@@ -1,58 +1,63 @@
 import Link from "next/link";
+import Head from "next/head";
 import { AiOutlineInstagram, AiFillYoutube } from "react-icons/ai";
 import { ImFacebook } from "react-icons/im";
 import { BsTwitter, BsSpotify, BsApple } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <main style={{ position: "absolute", width: "100vw", height: "100%" }}>
-      <div className="tiles-container">
-        <div className="checkers-bg-container">
-          <div className="checkers-bg"></div>
-        </div>
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <main style={{ position: "absolute", width: "100vw", height: "100%" }}>
+        <div className="tiles-container">
+          <div className="checkers-bg-container">
+            <div className="checkers-bg"></div>
+          </div>
+          <div className="color-rect">
+            <h3 className="jg-logo">JOEGARRATT</h3>
 
-        <div className="color-rect">
-          <h3 className="jg-logo">JOEGARRATT</h3>
-
-          <div className="list-container">
-            <ul>
-              <Link className="link" href="/listen">
-                <li>LISTEN</li>
-              </Link>
-              <Link className="link" href="/watch">
-                <li>WATCH</li>
-              </Link>
-              <li>TOUR</li>
-              <li>SHOP</li>
-              <Link className="link" href="/bio">
-                <li>BIO</li>
-              </Link>
-            </ul>
+            <div className="list-container">
+              <ul>
+                <Link className="link" href="/listen">
+                  <li>LISTEN</li>
+                </Link>
+                <Link className="link" href="/watch">
+                  <li>WATCH</li>
+                </Link>
+                <li>TOUR</li>
+                <li>SHOP</li>
+                <Link className="link" href="/bio">
+                  <li>BIO</li>
+                </Link>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="socials-container">
-        <ul style={{}} className="socials">
-          <li>
-            <AiOutlineInstagram size={40} />
-          </li>
-          <li>
-            <ImFacebook size={33} />
-          </li>
-          <li>
-            <BsTwitter size={33} />
-          </li>
-          <li>
-            <AiFillYoutube size={40} />
-          </li>
-          <li>
-            <BsSpotify size={33} />
-          </li>
-          <li>
-            <BsApple size={33} />
-          </li>
-        </ul>
-      </div>
-    </main>
+        <div className="socials-container">
+          <ul style={{}} className="socials">
+            <li>
+              <AiOutlineInstagram size={40} />
+            </li>
+            <li>
+              <ImFacebook size={33} />
+            </li>
+            <li>
+              <BsTwitter size={33} />
+            </li>
+            <li>
+              <AiFillYoutube size={40} />
+            </li>
+            <li>
+              <BsSpotify size={33} />
+            </li>
+            <li>
+              <BsApple size={33} />
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
   );
 }
